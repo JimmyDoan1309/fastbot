45e44a4a14f05651288bb40fe8d95fa6d8757c44
Trung Doan <trungdoan1309@gmail.com>
2021-07-19 12:49:54 +0700

    Add support for putting entities inside text for faster notation

--- src/fastbot/schema/nlu_data.py
+++ src/fastbot/schema/nlu_data.py
@@ -2,6 +2,8 @@ from . import BaseSchema
 from marshmallow import Schema, fields, validates_schema, validates, ValidationError
 from fastbot.models.nlu_data import Sample, NluData
 from fastbot.models.entity import Entity
+from fastbot.utils.common import parse_entities_from_text
+from marshmallow import post_load
 
 
 class EntitySchema(BaseSchema):
@@ -18,8 +20,18 @@ class EntitySchema(BaseSchema):
 
 class SampleSchema(BaseSchema):
     __cls_model__ = Sample
-    text = fields.String()
+    text = fields.String(required=True)
     entities = fields.List(fields.Nested(EntitySchema), default=[])
+
+    @post_load
+    def __convert__(self, data, **kwargs):
+        text = data.get('text')
+        entities = data.get('entities', [])
+        text, text_entities = parse_entities_from_text(text)
+        if text_entities:
+            text_entities = EntitySchema(many=True).load(text_entities)
+        entities.extend(text_entities)
+        return self.__cls_model__(text, entities)
 
 
 class NluDataSchema(BaseSchema):
--- src/fastbot/utils/common.py
+++ src/fastbot/utils/common.py
@@ -1,7 +1,8 @@
-from typing import Text
+import re
+import json
 
 
-def import_from_path(module_path: Text):
+def import_from_path(module_path: str):
     import importlib
     if "." in module_path:
         module_name, _, class_name = module_path.rpartition(".")
@@ -13,3 +14,32 @@ def import_from_path(module_path: Text):
             return module
         else:
             raise ImportError(f"Cannot retrieve class from path {module_path}.")
+
+
+def parse_entities_from_text(text: str):
+    regex = r"\[(?P<substring>[^\[\]]+)\](?P<config>\{[^\{\}]+\})"
+    extra_text_len = 0
+    entities = []
+    for match in re.finditer(regex, text):
+        group = match.groupdict()
+
+        entity_start = match.start(1)
+        entity_end = match.end(1)
+
+        config_text = group['config']
+        config = json.loads(config_text)
+
+        substring = group['substring']
+        full_text = match.group()
+
+        text = text.replace(full_text, substring)
+        entities.append({
+            **config,
+            'start': entity_start-extra_text_len,
+            'end': entity_end-extra_text_len})
+        extra_text_len += len(config_text)
+    for i, e in enumerate(entities):
+        e['start'] -= i*2+1
+        e['end'] -= i*2+1
+
+    return text, entities
