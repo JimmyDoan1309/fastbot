4f5f8b2b76f232dc74cc02c15962da2f52fa27e3
Trung Doan <trungdoan1309@gmail.com>
2021-08-06 20:02:30 +0700

    Enable CORS

--- fastbot-studio/backend/app/main.py
+++ fastbot-studio/backend/app/main.py
@@ -1,6 +1,13 @@
 from fastapi import FastAPI, Depends
+from fastapi.middleware.cors import CORSMiddleware
 from .routers import bot
 import uvicorn
 
 app = FastAPI()
 app.include_router(bot.router)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
