ad01f100217642e2fc130cc157249766337aea34
Trung Doan <trungdoan1309@gmail.com>
2021-08-07 18:15:09 +0700

    Add bare sample for

--- src/fastbot/cli/create.py
+++ src/fastbot/cli/create.py
@@ -3,7 +3,10 @@ import shutil
 import fastbot
 
 
-def run(path: str = './'):
+def run(path: str = './', bare: bool = False):
     path = os.path.abspath(path)
     root = os.path.dirname(fastbot.__file__)
-    shutil.copytree(f'{root}/sample', path, dirs_exist_ok=True)
+    if not bare:
+        shutil.copytree(f'{root}/sample/simple', path, dirs_exist_ok=True)
+    if bare:
+        shutil.copytree(f'{root}/sample/bare', path, dirs_exist_ok=True)
