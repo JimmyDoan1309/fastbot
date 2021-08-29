b5279fff14e25f52d196a8144c74632b7816c7eb
Quang Doan <doannhatquang27@gmail.com>
2021-08-29 12:59:16 +0700

    fe: fix: change sidebar node color

--- fastbot-studio/frontend/src/pages/BotFlowPage/Sidebar.tsx
+++ fastbot-studio/frontend/src/pages/BotFlowPage/Sidebar.tsx
@@ -1,4 +1,5 @@
 import React, { DragEvent } from "react";
+import "./sidebar.scss";
 
 const onDragStart = (event: DragEvent, nodeType: string) => {
   event.dataTransfer.setData("application/reactflow", nodeType);
@@ -12,14 +13,14 @@ const Sidebar = () => {
         You can drag these nodes to the pane on the left.
       </div>
       <div
-        className="react-flow__node-input"
+        className="react-flow__node-input intent-node"
         onDragStart={(event: DragEvent) => onDragStart(event, "intent")}
         draggable
       >
         Intent
       </div>
       <div
-        className="react-flow__node-default"
+        className="react-flow__node-default inputs-collector-node"
         onDragStart={(event: DragEvent) =>
           onDragStart(event, "inputsCollector")
         }
@@ -28,14 +29,14 @@ const Sidebar = () => {
         InputsCollector
       </div>
       <div
-        className="react-flow__node-default"
+        className="react-flow__node-default process-node"
         onDragStart={(event: DragEvent) => onDragStart(event, "process")}
         draggable
       >
         Process
       </div>
       <div
-        className="react-flow__node-output"
+        className="react-flow__node-output response-node"
         onDragStart={(event: DragEvent) => onDragStart(event, "response")}
         draggable
       >
